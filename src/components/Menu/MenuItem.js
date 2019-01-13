c0f8de9c98ff3707d7c837a6c7121f1d868ad141
Phanoix <phanoix@gmail.com>
2019-01-13 14:07:54 -0500

    menu item component init

---
+++ src/components/Menu/MenuItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MenuItem.css';
+
+class MenuItem extends Component {
+  render() {
+    return (
+      <div className="MenuItem">
+        
+      </div>
+    );
+  }
+}
+
+export default MenuItem;
