a667fcc6b7fa23a3105235754dbe301404672bd4
Ilia <phanoix@gmail.com>
2019-02-21 11:12:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Review time!
+            Hi! Review time!
           </p>
           <a
             className="App-link"
