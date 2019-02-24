f2daaa5dce895923edfaeda0b2154e2f3a4251dd
Phanoix <phanoix@gmail.com>
2019-02-24 16:52:30 -0500

    check for emptyh menu item lists

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,13 +3,22 @@ import './Menu.css';
 import MenuItem from './MenuItem';
 
 class Menu extends Component {
+  menuItems(menu_items) {
+    let list = [''];
+    if (menu_items){
+      list = menu_items.map((menu_item) => 
+        <MenuItem text={menu_item.toString()} key={menu_item.toString()}></MenuItem>
+      )
+    }
+
+    return list;
+  }
+
   render() {
     const menu_items = this.props.menu_items;
     return (
       <div className="Menu">
-        {menu_items.map((menu_item) => 
-          <MenuItem text={menu_item.toString()}></MenuItem>
-        )}
+        {this.menuItems(menu_items)}
       </div>
     );
   }
