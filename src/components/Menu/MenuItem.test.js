b28eb0cf0dba264e75b96687bdfd402c2315fd0a
Phanoix <phanoix@gmail.com>
2019-02-24 17:13:16 -0500

    added menu item text rendering test

--- src/components/Menu/MenuItem.test.js
+++ src/components/Menu/MenuItem.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './Menu';
+import MenuItem from './MenuItem';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Menu />, div);
+  ReactDOM.render(<MenuItem />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('displays the menu item text', () => {
+  const text = "test123";
+  const div = document.createElement('div');
+  ReactDOM.render(<MenuItem text={text} />, div);
+  expect(div.querySelector('div').textContent).toBe(text);
   ReactDOM.unmountComponentAtNode(div);
 });
