5a4e8921f731bf1291b425790d7927be1c19eb70
Phanoix <phanoix@gmail.com>
2020-01-05 14:55:22 -0500

    added some colours

--- src/components/Grid/GridSquare.js
+++ src/components/Grid/GridSquare.js
@@ -4,10 +4,22 @@ import './GridSquare.css';
 class GridSquare extends Component {
   render() {
     return (
-      <div className="GridSquare">
+      <div className={"GridSquare " + this.getContextSensitiveClasses(this.props.text) } >
         {this.props.text}
       </div>
     );
+  }
+
+  getContextSensitiveClasses( text ){
+    const classes = [];
+    classes['w'] = 'Wall';
+    classes['0'] = 'Floor';
+    classes['e'] = 'Entrance';
+    classes['x'] = 'Exit';
+    classes[1] = 'Character';
+    classes[-1] = 'CharacterPath';
+
+    return classes[text];
   }
 }
 
